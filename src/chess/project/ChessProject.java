b2a4e39115422baa3bc6b23d5f3c30b678b4657f
Alexandru MIHAI <alexandru.mihai0808@cti.pub.ro>
2014-03-01 10:45:59 +0200

    undo test

--- src/chess/project/ChessProject.java
+++ src/chess/project/ChessProject.java
@@ -7,8 +7,6 @@ public class ChessProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Aici a scris alex");
-        
     }
     
 }
