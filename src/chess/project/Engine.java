dcd79a8e01b17d250b1c93f8c881c9730e13d592
Selennae <anca.andreea.rosca@gmail.com>
2014-03-08 21:33:38 +0200

    test Anca

--- src/chess/project/Engine.java
+++ src/chess/project/Engine.java
@@ -14,6 +14,7 @@ package chess.project;
  */
 public class Engine {
 
+    static String white = new String();
     static void resetHash() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
