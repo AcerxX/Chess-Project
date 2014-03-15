e7b1c87cdcb0ddbaba8193333c85e936e8e436f8
Alexandru MIHAI <alexandru.mihai0808@cti.pub.ro>
2014-03-15 18:08:24 +0200

    [v0.3.2a] Cleanup Main.java

--- src/chess/project/Main.java
+++ src/chess/project/Main.java
@@ -10,14 +10,13 @@ package chess.project;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
  * 
- * @version 0.3a Etapa 1 FINAL
+ * @version 0.3.2a Etapa 1 FINAL
  * @author Alexandru MIHAI
  */
 public class Main {
@@ -35,39 +34,13 @@ public class Main {
     public static int i50MovesRule;
     public static int iTimeOut;
     
-    public static String latestMoves;
-    
     
     
     public static void main(String args[]) throws IOException{
         Board.initAll();
-        try{
-            reader = new BufferedReader(new InputStreamReader(System.in));
-            latestMoves = "none";
-            printCopywright();
-            getCmd();
-        } catch(IOException ex){
-            File f = new File("error");
-            System.out.println(f.getCanonicalPath());
-            f.createNewFile();
-            FileWriter fileWriter;
-            BufferedWriter buffWriter;
-            try{
-                fileWriter = new FileWriter(f);
-                buffWriter = new BufferedWriter(fileWriter);
-                buffWriter.write(latestMoves + "\n");
-                buffWriter.write(ex.toString()+"\n");
-                StackTraceElement[] arrTrace = ex.getStackTrace();
-                int stackSize = arrTrace.length;
-                for(int i=0; i<stackSize; i++)
-                {
-                        buffWriter.write(arrTrace[i].toString()+"\n");
-                }
-                buffWriter.close();
-            } catch(IOException ex2) {ex2.printStackTrace(System.out);}
-
-            System.out.println(ex);
-        }            
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        printCopywright();
+        getCmd();        
     }
     
     /**
