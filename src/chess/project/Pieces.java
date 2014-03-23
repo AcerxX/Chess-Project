c6e2a35f0ca61e134c6a8e6f6ba4e783f1cfe80d
AndreeaIlies <andreea53171@gmail.com>
2014-03-23 02:16:12 +0200

    inceput etapa 2

---
+++ src/chess/project/Pieces.java
@@ -0,0 +1,33 @@
+/*
+ * Copywright (C) [2014] [Alexandru MIHAI] [Andreea ILIES] [Teodora TANASE] [Anca ROSCA]
+ * 
+ * This file is part of Thunder Chickens Chess Project.
+ * 
+ * Title: The Chess Project of Team <Thunder Chickens> @ CS, PUB, RO. 
+ */
+
+package chess.project;
+
+/**
+ *
+ * @author andre_000
+ */
+public class Pieces {
+    String type;
+    
+    /**
+     * LEGAL DECLARATIONS:
+     * - pion
+     * - dama
+     * - rege
+     * - nebun
+     * - tura
+     * - cal
+     * 
+     * @param pc 
+     */
+    public Pieces(String pc){
+        type = pc;
+        //TODO declarari gresite
+    }
+}
